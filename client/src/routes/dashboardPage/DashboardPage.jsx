d65997eacb3c049a6a681c2d05584257596a626f
gabrielsilva <gabrielsilva1995@gmail.com>
2025-02-22 22:33:58 +1100

    Update DashboardPage.jsx with latest changes

--- client/src/routes/dashboardPage/DashboardPage.jsx
+++ client/src/routes/dashboardPage/DashboardPage.jsx
@@ -1,5 +1,5 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
-import "./dashboardLayout.css";
+import "./dashboardPage.css";
 import { useNavigate } from "react-router-dom";
 
 const DashboardPage = () => {
